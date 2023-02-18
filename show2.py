7ee98fbfed030019235b2c7a265f686dd3f3e474
Yi Zhen <zhenyisx@gmail.com>
2023-02-18 11:43:27 -0800

    Create show2.py

---
+++ show2.py
@@ -0,0 +1,28 @@
+# streamlit_app.py
+
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
+# Retrieve file contents.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
+
+bucket_name = "scoutoid-streamlit-bucket"
+file_path = "schedule_tmp.csv"
+
+content = read_file(bucket_name, file_path)
+
+# Print results.
+for line in content.strip().split("\n"):
+    st.write(line)
